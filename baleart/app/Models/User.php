cd7a591500ce4ff4ce9cbb51d76d163a0d0c95c2
Adrián Gómez Perelló <agp00@iesemilidarder.com>
2024-12-25 11:10:13 +0100

    auth token

--- baleart/app/Models/User.php
+++ baleart/app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use App\Models\Role;
 use App\Models\Space;
 use App\Models\Comment;
+use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -13,7 +14,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, HasApiTokens, Notifiable;
 
     /**
      * The attributes that are mass assignable.
--- baleart/routes/api.php
+++ baleart/routes/api.php
@@ -7,9 +7,6 @@ use App\Http\Controllers\Api\SpaceController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
-Route::middleware(['auth:sanctum'])->get('users', function (Request $request) {
-    return $request->user();
-});
 
 Route::post('register', [RegisteredUserController::class, 'store']);
 
@@ -17,26 +14,24 @@ Route::post('login', [AuthenticatedSessionController::class, 'store']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthenticatedSessionController::class, 'destroy']);
+
+
+    Route::get('users', [UserController::class, 'index']);
+
+    Route::put('users/{identifier}', [UserController::class, 'update']);
+
+    Route::delete('users/{email}', [UserController::class, 'destroy']);
+
+    Route::get('users/{email}', [UserController::class, 'show']);
+
+
+
+    Route::post('spaces/{RegNumber}', [SpaceController::class, 'store']);
+
+    Route::get('spaces', [SpaceController::class, 'index']);
+
+    //Route::get('spaces/{illa}', [SpaceController::class, 'index']);
+
+    Route::get('spaces/{identifier}', [SpaceController::class, 'show']);
 });
-
-
-
-
-Route::get('users', [UserController::class, 'index']);
-
-Route::put('users/{identifier}', [UserController::class, 'update']);
-
-Route::delete('users/{email}', [UserController::class, 'destroy']);
-
-Route::get('users/{email}', [UserController::class, 'show']);
-
-
-
-Route::post('spaces/{RegNumber}', [SpaceController::class, 'store']);
-
-Route::get('spaces', [SpaceController::class, 'index']);
-
-//Route::get('spaces/{illa}', [SpaceController::class, 'index']);
-
-Route::get('spaces/{identifier}', [SpaceController::class, 'show']);
 
